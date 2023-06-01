4c74873b3a23cf1d9e7b2641ebb8b6ec5fb86594
Wisdom Mahlangu <wizzymahlangu@gmail.com>
2023-06-01 14:59:01 +0200

    first commit

---
+++ src/test/java/com/GroupMockUp/MockUp/MockUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GroupMockUp.MockUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
