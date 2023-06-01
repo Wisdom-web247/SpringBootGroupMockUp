3b1d86808ae0b5bb3462d79d8f5c770e101cce9f
Wisdom Mahlangu <wizzymahlangu@gmail.com>
2023-06-01 15:18:03 +0200

    Added 2 imports

---
+++ src/main/java/com/GroupMockUp/MockUp/Controller/MockUpController.java
@@ -0,0 +1,15 @@
+package com.GroupMockUp.MockUp.Controller;
+
+//Imports
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:4200")
+//Specify that this is a REST api
+@RestController
+
+public class MockUpController {
+    
+}
