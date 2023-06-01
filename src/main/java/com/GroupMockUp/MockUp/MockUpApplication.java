222b1207e3efe9195b2a0b481d4d0d9bb9d46be8
Tsumbedzo_Nevhutalu <vanessatn96@gmail.com>
2023-06-01 15:46:03 +0200

    practice to git added a print statement to the model

--- src/main/java/com/GroupMockUp/MockUp/MockUpApplication.java
+++ src/main/java/com/GroupMockUp/MockUp/MockUpApplication.java
@@ -5,9 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MockUpApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(MockUpApplication.class, args);
+		
+		System.out.println("Hello world");
+		System.out.println("Hello world");
 	}
+	
 
 }
+
